fb0c5dd1e49a1880a73be75e0c586aeecb24ab32
Lionel Pointet <lionel.pointet@gmail.com>
2014-06-24 21:28:50 +0200

    Fix #1 Github: declare statically used method as such

--- wp-app-kit/lib/web-services/web-services-crud.php
+++ wp-app-kit/lib/web-services/web-services-crud.php
@@ -6,49 +6,49 @@ require_once(dirname(__FILE__) .'/core-web-services/component.php');
 
 class WpakWebServiceCrud{
 
-	public function create($app_id,$service_slug,$data){
-		
+	public static function create($app_id,$service_slug,$data){
+
 		$service_answer = array();
 		$service_answer = apply_filters('wpak_create_'. $service_slug,$service_answer,$data,$app_id);
 		$service_answer = apply_filters('wpak_create',$service_answer,$service_slug,$data,$app_id);
-		
+
 		return $service_answer;
 	}
-	
-	public function read($app_id,$service_slug,$query_vars){
-		
+
+	public static function read($app_id,$service_slug,$query_vars){
+
 		$service_answer = array();
 		$service_answer = apply_filters('wpak_read_'. $service_slug,$service_answer,$query_vars,$app_id);
 		$service_answer = apply_filters('wpak_read',$service_answer,$service_slug,$query_vars,$app_id);
-		
+
 		return $service_answer;
 	}
-	
-	public function read_one($app_id,$service_slug,$id){
+
+	public static function read_one($app_id,$service_slug,$id){
 
 		$service_answer = array();
 		$service_answer = apply_filters('wpak_read_one_'. $service_slug,$service_answer,$id,$app_id);
 		$service_answer = apply_filters('wpak_read_one',$service_answer,$service_slug,$id,$app_id);
-		
+
 		return $service_answer;
 	}
-	
-	public function update($app_id,$service_slug,$data){
-		
+
+	public static function update($app_id,$service_slug,$data){
+
 		$service_answer = array();
 		$service_answer = apply_filters('wpak_update_'. $service_slug,$service_answer,$id,$app_id);
 		$service_answer = apply_filters('wpak_update',$service_answer,$service_slug,$id,$app_id);
-		
+
 		return $service_answer;
 	}
-	
-	public function delete($app_id,$service_slug,$id){
+
+	public static function delete($app_id,$service_slug,$id){
 
 		$service_answer = array();
 		$service_answer = apply_filters('wpak_delete_'. $service_slug,$service_answer,$id,$app_id);
 		$service_answer = apply_filters('wpak_delete',$service_answer,$service_slug,$id,$app_id);
-		
+
 		return $service_answer;
 	}
-	
+
 }
