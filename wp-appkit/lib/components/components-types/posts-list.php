29a801741591a8ef25112cf6a1d4bd4068c2d801
Mathieu Le Roi <code@mathieuleroi.com>
2015-05-08 23:29:36 +0200

    Fix #50 "Post list" component : don't force to choose a taxonomy term

--- wp-appkit/lib/components/components-types/posts-list.php
+++ wp-appkit/lib/components/components-types/posts-list.php
@@ -71,19 +71,28 @@ class WpakComponentTypePostsList extends WpakComponentType {
 
 				$query['type'] = 'last-posts';
 
-			}elseif ( !empty( $options['taxonomy'] ) && !empty( $options['term'] ) ) {
+			}elseif ( !empty( $options['taxonomy'] ) ) {
 
-				$query_args['tax_query'] = array(
-					array(
-						'taxonomy' => $options['taxonomy'],
-						'field' => 'slug',
-						'terms' => $options['term']
-					)
-				);
+				if ( $options['taxonomy'] === 'wpak-none' ) {
+					
+					$query['type'] = 'post-type';
+					
+				} elseif ( !empty( $options['term'] ) ) {
+					
+					$query_args['tax_query'] = array(
+						array(
+							'taxonomy' => $options['taxonomy'],
+							'field' => 'slug',
+							'terms' => $options['term']
+						)
+					);
 
-				$query['type'] = 'taxonomy';
-				$query['taxonomy'] = $options['taxonomy'];
-				$query['terms'] = is_array( $options['term'] ) ? $options['term'] : array( $options['term'] );
+					$query['type'] = 'taxonomy';
+					$query['taxonomy'] = $options['taxonomy'];
+					$query['terms'] = is_array( $options['term'] ) ? $options['term'] : array( $options['term'] );
+					
+				}
+				
 			}
 
 			if ( !empty( $before_post_date ) ) {
@@ -206,17 +215,28 @@ class WpakComponentTypePostsList extends WpakComponentType {
 			$options = array(
 				'hook' => array( 'label' => __( 'Hook', WpAppKit::i18n_domain ), 'value' => $component->options['hook'] ),
 			);
-		} elseif ( $component->options['post-type'] != 'last-posts' ) {
+		} elseif ( $component->options['post-type'] == 'last-posts'  ) {
+			$options = array(
+				'post-type' => array( 'label' =>  __( 'List type', WpAppKit::i18n_domain ), 'value' => __( 'Latest posts', WpAppKit::i18n_domain ) )
+			);
+		} elseif ( !empty ( $component->options['post-type'] ) ) {
 			$post_type = get_post_type_object( $component->options['post-type'] );
-			$taxonomy = get_taxonomy( $component->options['taxonomy'] );
-			$term = get_term_by( 'slug', $component->options['term'], $component->options['taxonomy'] );
-			$options = array();
-			if ( !is_wp_error( $term ) ) {
-				$options = array(
-					'post-type' => array( 'label' => __( 'Post type' ), 'value' => $post_type->labels->name ),
-					'taxonomy' => array( 'label' => __( 'Taxonomy' ), 'value' => $taxonomy->labels->name ),
-					'term' => array( 'label' => __( 'Term' ), 'value' => $term->name )
-				);
+			$taxo_name = '';
+			$term_name = '';
+			if ( !empty( $component->options['taxonomy'] ) && $component->options['taxonomy'] !== 'wpak-none' ) {
+				$taxonomy = get_taxonomy( $component->options['taxonomy'] );
+				$term = get_term_by( 'slug', $component->options['term'], $component->options['taxonomy'] );
+				if ( !is_wp_error( $term ) ) {
+					$taxo_name = $taxonomy->labels->name;
+					$term_name = $term->name;
+				}
+			}
+			$options = array(
+				'post-type' => array( 'label' => __( 'Post type' ), 'value' => $post_type->labels->name )
+			);
+			if ( !empty( $taxo_name ) ) {
+				$options['taxonomy'] = array( 'label' => __( 'Taxonomy' ), 'value' => $taxo_name );
+				$options['term'] = array( 'label' => __( 'Term' ), 'value' => $term_name );
 			}
 		}
 		return $options;
@@ -311,12 +331,13 @@ class WpakComponentTypePostsList extends WpakComponentType {
 				 */
 				$taxonomies = apply_filters( 'wpak_component_type_posts_list_form_taxonomies', $taxonomies );
 
-				$first_taxonomy = reset( $taxonomies );
-				$current_taxonomy = empty( $current_taxonomy ) ? $first_taxonomy : $current_taxonomy;
+				$first_taxonomy = !empty( $taxonomies ) ? reset( $taxonomies ) : '';
+				$current_taxonomy = empty( $current_taxonomy ) ? empty( $first_taxonomy ) ? 'wpak-none' : $first_taxonomy : $current_taxonomy;
 			?>
 			<label><?php _e( 'Taxonomy', WpAppKit::i18n_domain ) ?> : </label>
 			<?php if ( !empty( $taxonomies ) ): ?>
 				<select name="taxonomy" class="posts-list-taxonomies">
+					<option value="wpak-none" <?php echo $current_taxonomy == 'wpak-none' ? 'selected="selected"' : '' ?>><?php _e( 'No taxonomy', WpAppKit::i18n_domain ) ?></option>
 					<?php foreach ( $taxonomies as $taxonomy_slug ): ?>
 						<?php $taxonomy = get_taxonomy( $taxonomy_slug ) ?>
 						<?php $selected = $taxonomy_slug == $current_taxonomy ? 'selected="selected"' : '' ?>
@@ -324,20 +345,22 @@ class WpakComponentTypePostsList extends WpakComponentType {
 					<?php endforeach ?>
 				</select>
 				<br/>
-				<?php
-					$taxonomy_obj = get_taxonomy( $current_taxonomy );
-					$terms = get_terms( $current_taxonomy );
-				?>
-				<label><?php echo $taxonomy_obj->labels->name ?> : </label>
-				<?php if ( !empty( $terms ) ): ?>
-					<select name="term">
-						<?php foreach ( $terms as $term ): ?>
-							<?php $selected = $term->slug == $current_term ? 'selected="selected"' : '' ?>
-							<option value="<?php echo $term->slug ?>" <?php echo $selected ?>><?php echo $term->name ?></option>
-						<?php endforeach ?>
-					</select>
-				<?php else: ?>
-					<?php echo sprintf( __( 'No %s found', WpAppKit::i18n_domain ), $taxonomy_obj->labels->name ); ?>
+				<?php if ( $current_taxonomy !== 'wpak-none' ) : ?>
+					<?php
+						$taxonomy_obj = get_taxonomy( $current_taxonomy );
+						$terms = get_terms( $current_taxonomy );
+					?>
+					<label><?php echo $taxonomy_obj->labels->name ?> : </label>
+					<?php if ( !empty( $terms ) ): ?>
+						<select name="term">
+							<?php foreach ( $terms as $term ): ?>
+								<?php $selected = $term->slug == $current_term ? 'selected="selected"' : '' ?>
+								<option value="<?php echo $term->slug ?>" <?php echo $selected ?>><?php echo $term->name ?></option>
+							<?php endforeach ?>
+						</select>
+					<?php else: ?>
+						<?php echo sprintf( __( 'No %s found', WpAppKit::i18n_domain ), $taxonomy_obj->labels->name ); ?>
+					<?php endif ?>
 				<?php endif ?>
 			<?php else: ?>
 				<?php echo sprintf( __( 'No taxonomy found for post type %s', WpAppKit::i18n_domain ), $current_post_type ); ?>
