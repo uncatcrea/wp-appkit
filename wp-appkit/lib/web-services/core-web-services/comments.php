b505a19be5a355a25de4ed57dcecfa3f60895b29
Mathieu Le Roi <code@mathieuleroi.com>
2015-07-09 16:31:16 +0200

    Start implementing web service for comment creation

--- wp-appkit/lib/web-services/core-web-services/comments.php
+++ wp-appkit/lib/web-services/core-web-services/comments.php
@@ -4,6 +4,7 @@ class WpakWebServiceComments {
 
 	public static function hooks() {
 		add_filter( 'wpak_read_one_comments-post', array( __CLASS__, 'read_one' ), 10, 3 );
+		add_filter( 'wpak_create_comments-post', array( __CLASS__, 'create' ), 10, 3 );
 	}
 
 	public static function read_one( $service_answer, $id, $app_id ) {
@@ -101,6 +102,115 @@ class WpakWebServiceComments {
 		return $tree;
 	}
 
+	public static function create( $service_answer, $data, $app_id ) {
+		$service_answer = array();
+		
+		$service_answer['comment_ok'] = false;
+		
+		if ( !empty( $data['comment'] ) ) {
+			
+			//Check authentication
+			if ( !empty( $data['auth'] ) && !empty( $data['auth']['type'] ) ) {
+				
+				
+				
+			}else {
+				$service_answer['comment_error'] = 'no-auth';
+			}
+			
+		} else {
+			$service_answer['comment_error'] = 'no-comment';
+		}
+		
+		/*
+		if ( ! empty( $request['id'] ) ) {
+			return new WP_Error( 'rest_comment_exists', __( 'Cannot create existing comment.' ), array( 'status' => 400 ) );
+		}
+		$post = get_post( $request['post'] );
+		if ( empty( $post ) ) {
+			return new WP_Error( 'rest_post_invalid_id', __( 'Invalid post ID.' ), array( 'status' => 404 ) );
+		}
+		$prepared_comment = $this->prepare_item_for_database( $request );
+		// Setting remaining values before wp_insert_comment so we can
+		// use wp_allow_comment().
+		$prepared_comment['comment_author_IP'] = '127.0.0.1';
+		$prepared_comment['comment_agent'] = '';
+		$prepared_comment['comment_approved'] = wp_allow_comment( $prepared_comment );
+		$prepared_comment = apply_filters( 'rest_pre_insert_comment', $prepared_comment, $request );
+		$comment_id = wp_insert_comment( $prepared_comment );
+		if ( ! $comment_id ) {
+			return new WP_Error( 'rest_comment_failed_create', __( 'Creating comment failed.' ), array( 'status' => 500 ) );
+		}
+		if ( isset( $request['status'] ) ) {
+			$comment = get_comment( $comment_id );
+			$this->handle_status_param( $request['status'], $comment );
+		}
+		$this->update_additional_fields_for_object( get_comment( $comment_id ), $request );
+		$context = current_user_can( 'moderate_comments' ) ? 'edit' : 'view';
+		$response = $this->get_item( array(
+			'id'      => $comment_id,
+			'context' => $context,
+		) );
+		$response = rest_ensure_response( $response );
+		if ( is_wp_error( $response ) ) {
+			return $response;
+		}
+		$response->set_status( 201 );
+		$response->header( 'Location', rest_url( '/wp/v2/comments/' . $comment_id ) );
+		return $response;
+		*/
+		
+		return (object)$service_answer;
+	}
+	
+	protected function prepare_item_for_database( $request ) {
+		$prepared_comment = array();
+		if ( isset( $request['content'] ) ) {
+			$prepared_comment['comment_content'] = $request['content'];
+		}
+		if ( isset( $request['post'] ) ) {
+			$prepared_comment['comment_post_ID'] = (int) $request['post'];
+		}
+		if ( isset( $request['parent'] ) ) {
+			$prepared_comment['comment_parent'] = $request['parent'];
+		}
+		if ( isset( $request['author'] ) ) {
+			$prepared_comment['user_id'] = $request['author'];
+		}
+		if ( isset( $request['author_name'] ) ) {
+			$prepared_comment['comment_author'] = $request['author_name'];
+		}
+		if ( isset( $request['author_email'] ) ) {
+			$prepared_comment['comment_author_email'] = $request['author_email'];
+		}
+		if ( isset( $request['author_url'] ) ) {
+			$prepared_comment['comment_author_url'] = $request['author_url'];
+		}
+		if ( isset( $request['type'] ) ) {
+			$prepared_comment['comment_type'] = $request['type'];
+		}
+		if ( isset( $request['karma'] ) ) {
+			$prepared_comment['comment_karma'] = $request['karma'] ;
+		}
+		if ( ! empty( $request['date'] ) ) {
+			$date_data = rest_get_date_with_gmt( $request['date'] );
+			if ( ! empty( $date_data ) ) {
+				list( $prepared_comment['comment_date'], $prepared_comment['comment_date_gmt'] ) =
+					$date_data;
+			} else {
+				return new WP_Error( 'rest_invalid_date', __( 'The date you provided is invalid.' ), array( 'status' => 400 ) );
+			}
+		} elseif ( ! empty( $request['date_gmt'] ) ) {
+			$date_data = rest_get_date_with_gmt( $request['date_gmt'], true );
+			if ( ! empty( $date_data ) ) {
+				list( $prepared_comment['comment_date'], $prepared_comment['comment_date_gmt'] ) = $date_data;
+			} else {
+				return new WP_Error( 'rest_invalid_date', __( 'The date you provided is invalid.' ), array( 'status' => 400 ) );
+			}
+		}
+		return apply_filters( 'rest_preprocess_comment', $prepared_comment, $request );
+	}
+	
 }
 
 WpakWebServiceComments::hooks();
