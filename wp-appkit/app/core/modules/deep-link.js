dbc86c1d2651485f86d57a087b3479890380055e
Lionel Pointet <lionel.pointet@virtua.ch>
2015-07-29 20:02:53 +0200

    Add a library to parse an URI and use it into DeepLink module to guess the launch route

--- wp-appkit/app/core/modules/deep-link.js
+++ wp-appkit/app/core/modules/deep-link.js
@@ -1,12 +1,20 @@
 define( function( require ) {
     "use strict";
 
+    var ParseURI = require( 'core/lib/parse-uri' );
+
     var deepLink = {};
 
     deepLink.getLaunchRoute = function( launch_route ) {
         if( wpak_open_url.length ) {
             // Parse URL
-            alert( 'in getLaunchRoute ' + wpak_open_url );
+            var parsed_url = ParseURI.parse( wpak_open_url );
+
+            if( parsed_url.host.length > 0 ) {
+                launch_route = '#' + parsed_url.host + '/' + parsed_url.path;
+
+                // TODO: handle query string (stored into parsed_url.query)
+            }
         }
 
         return launch_route;
