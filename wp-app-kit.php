7e0a3213a47eb1a313f17c5855c7af122379dcf3
Mathieu Le Roi <code@mathieuleroi.com>
2015-10-06 11:08:10 +0200

    Allow to git checkout directly the root of wp-appkit repository

---
+++ wp-app-kit.php
@@ -0,0 +1,8 @@
+<?php
+/*
+  Plugin Name: WP AppKit
+  Description: Build Phonegap Mobile apps based on your WordPress content
+  Version: 0.4.1
+ */
+
+require_once(dirname(__FILE__) .'/wp-appkit/wp-appkit.php');
