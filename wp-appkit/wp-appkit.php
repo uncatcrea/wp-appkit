2d96f591f541d441cc3c52bf18899ea0f857df79
Mathieu Le Roi <mathleroi@gmail.com>
2017-02-06 10:19:00 +0100

    Update changelog and version to 0.6.2

--- wp-appkit/wp-appkit.php
+++ wp-appkit/wp-appkit.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-AppKit
 Plugin URI:  https://github.com/uncatcrea/wp-appkit
 Description: Build Phonegap Mobile apps based on your WordPress content.
-Version:     0.6.1
+Version:     0.6.2
 Author:      Uncategorized Creations
 Author URI:  http://getwpappkit.com
 Text Domain: wp-appkit
@@ -19,7 +19,7 @@ if ( !class_exists( 'WpAppKit' ) ) {
 
 	class WpAppKit {
 
-		const resources_version = '0.6.1';
+		const resources_version = '0.6.2';
 		const i18n_domain = 'wp-appkit';
 
 		public static function hooks() {
