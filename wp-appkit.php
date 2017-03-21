7cc9f7c8e72bb3d2d96de7eb5763d94116c0a961
Mathieu Le Roi <mathleroi@gmail.com>
2017-03-21 17:51:24 +0100

    Update version > 1.0

--- wp-appkit.php
+++ wp-appkit.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-AppKit
 Plugin URI:  https://github.com/uncatcrea/wp-appkit
 Description: Build Phonegap Mobile apps based on your WordPress content.
-Version:     0.6.2
+Version:     1.0
 Author:      Uncategorized Creations
 Author URI:  http://getwpappkit.com
 Text Domain: wp-appkit
--- wp-appkit/wp-appkit.php
+++ wp-appkit/wp-appkit.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-AppKit
 Plugin URI:  https://github.com/uncatcrea/wp-appkit
 Description: Build Phonegap Mobile apps based on your WordPress content.
-Version:     0.6.2
+Version:     1.0
 Author:      Uncategorized Creations
 Author URI:  http://getwpappkit.com
 Text Domain: wp-appkit
@@ -20,7 +20,7 @@ if ( !class_exists( 'WpAppKit' ) ) {
 
 	class WpAppKit {
 
-		const resources_version = '0.6.2';
+		const resources_version = '1.0';
 		const i18n_domain = 'wp-appkit';
 
 		public static function hooks() {
