b5d83f4472bf108fb345f8d3a6fa8562f82336d2
Mathieu Le Roi <mathleroi@gmail.com>
2017-05-09 11:57:01 +0200

    Set uncatcrea site urls to preprod

--- wp-appkit/lib/config.php
+++ wp-appkit/lib/config.php
@@ -1,9 +1,9 @@
 <?php
 
 class WpakConfig {
-    const uncatcrea_website_url = 'https://local.uncategorized-creations.com';
-    const support_url = 'https://local.uncategorized-creations.com/contact-us/';
-    const my_account_url = 'https://local.uncategorized-creations.com/my-account/';
-    const checkout_url = 'https://local.uncategorized-creations.com/checkout/';
+    const uncatcrea_website_url = 'https://uncategorized-creations.com/preprod/';
+    const support_url = 'https://uncategorized-creations.com/preprod/contact-us/';
+    const my_account_url = 'https://uncategorized-creations.com/preprod/my-account/';
+    const checkout_url = 'https://uncategorized-creations.com/preprod/checkout/';
 }
 
