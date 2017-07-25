363798642b87b33ae8609078e059385147864ad3
Mathieu Le Roi <mathleroi@gmail.com>
2017-07-25 14:16:59 +0200

    Fix #323 Wrong routing initialization when no network at first app launch

--- wp-appkit/app/core/launch.js
+++ wp-appkit/app/core/launch.js
@@ -116,7 +116,7 @@ require(['root/config'],function(Config){
 																});
 															},
 															function( error ){
-																Backbone.history.start();
+																App.launchRouting();
 
 																var error_message = "Error : App could not synchronize with website";
 
