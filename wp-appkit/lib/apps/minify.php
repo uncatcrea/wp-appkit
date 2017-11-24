6965425edac231fcabb33bcb03b6cd1687859950
Mathieu Le Roi <mathleroi@gmail.com>
2017-11-24 17:06:32 +0100

    JS Minification

---
+++ wp-appkit/lib/apps/minify.php
@@ -0,0 +1,36 @@
+<?php
+
+class WpakMinify {
+    
+    protected $jsqueeze = null;
+    
+    public function __construct() {
+        require_once( dirname( __FILE__ ) . '/../../vendor/minification/js/JSqueeze.php' );
+        $this->jsqueeze = new Patchwork\JSqueeze();
+    }
+    
+    public function minify( $file_extension, $content_to_minify ) {
+        $minified = $content_to_minify;
+        
+        $method = 'minify_'. $file_extension;
+        if ( method_exists( $this, $method ) ) {
+            $minified = $this->{$method}( $content_to_minify );
+        }
+        
+        return $minified;
+    }
+    
+    public function minify_js( $content_to_minify ) {
+        
+        $minified_content = $this->jsqueeze->squeeze(
+            $content_to_minify,
+            true,   // singleLine
+            false,   // keepImportantComments
+            false   // specialVarRx
+        );
+        
+        return $minified_content;
+    }
+    
+}
+
