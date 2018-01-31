e5becaf2a1f82aeb30faa367b4c3b2562ab572ce
Mathieu Le Roi <mathleroi@gmail.com>
2018-01-31 22:32:17 +0100

    Fix missing trailing slash when coming back to root fragment

--- wp-appkit/app/core/router.js
+++ wp-appkit/app/core/router.js
@@ -48,6 +48,68 @@ define(function (require, exports) {
         return route_type;
     };
 
+    /**
+     * Override Backbone.history.navigate so that final slash is not stripped on root fragment.
+     * This problem is related in this unresolved Backbone issue: https://github.com/jashkenas/backbone/issues/3391
+     * Proposed solution is here translated to Backbone v1.3.3's navigate function:
+     */
+    _.extend(Backbone.history, {
+        navigate: function(fragment, options) {
+
+            //Added for WPAK
+            var pathStripper = /#.*$/;
+
+            if (!Backbone.History.started) return false;
+            if (!options || options === true) options = {trigger: !!options};
+
+            // Normalize the fragment.
+            fragment = this.getFragment(fragment || '');
+
+            // Don't include a trailing slash on the root.
+            var rootPath = this.root;
+            if (fragment === '' || fragment.charAt(0) === '?') {
+                //Commented for WPAK so that final slash is not stripped on root fragment
+                //rootPath = rootPath.slice(0, -1) || '/';
+            }
+            var url = rootPath + fragment;
+
+            // Strip the hash and decode for matching.
+            fragment = this.decodeFragment(fragment.replace(pathStripper, ''));
+
+            if (this.fragment === fragment) return;
+            this.fragment = fragment;
+
+            // If pushState is available, we use it to set the fragment as a real URL.
+            if (this._usePushState) {
+            this.history[options.replace ? 'replaceState' : 'pushState']({}, document.title, url);
+
+            // If hash changes haven't been explicitly disabled, update the hash
+            // fragment to store history.
+            } else if (this._wantsHashChange) {
+                this._updateHash(this.location, fragment, options.replace);
+                if (this.iframe && fragment !== this.getHash(this.iframe.contentWindow)) {
+                    var iWindow = this.iframe.contentWindow;
+
+                    // Opening and closing the iframe tricks IE7 and earlier to push a
+                    // history entry on hash-tag change.  When replace is true, we don't
+                    // want this.
+                    if (!options.replace) {
+                        iWindow.document.open();
+                        iWindow.document.close();
+                    }
+
+                    this._updateHash(iWindow.location, fragment, options.replace);
+                }
+
+            // If you've told us that you explicitly don't want fallback hashchange-
+            // based history, then `navigate` becomes a page refresh.
+            } else {
+                return this.location.assign(url);
+            }
+            if (options.trigger) return this.loadUrl(fragment);
+        }
+    });
+
     return Backbone.Router.extend({
 
         routes: {
